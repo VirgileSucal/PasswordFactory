7c8d592ded7149455b0e44981f5bb5c1b64ad3aa
uapv1705019 <laurent.pereira-da-silva-quintas@alumni.univ-avignon.fr>
2021-12-06 12:33:51 +0100

    Added data extraction for password generation
    train and validation dataset are read from files.
    A small train dataset has also been generated to allow fast code debugging.
    
    Updated files:
    	password_generation.py

--- src/password_generation.py
+++ src/password_generation.py
@@ -1,7 +1,12 @@
 #! /bin/env python3
 
 import torch
+import string
+import consts
 
+from nameGeneration import *
+from tools import *
+from data_mining import *
 
 if torch.cuda.is_available():
     device = torch.device("cuda:0")
@@ -11,4 +16,10 @@ else:
     print('ONLY CPU AVAILABLE')
 
 
+if __name__ == '__main__':
 
+    train_set, eval_set = extract_data()
+    small_train_set = train_set[-1000:]
+    # print('train_set: ', len(train_set))
+    # print('eval_set: ', len(eval_set))
+    # print(small_train_set)
