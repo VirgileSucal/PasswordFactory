5194a717ed512fc24d3e3cbfc4f4e26ad2b58ee0
Virgile Sucal <virgile.sucal@alumni.univ-avignon.fr>
2021-12-06 10:38:39 +0100

    Add password_generation.py
    Added File:
    	password_generation.py

---
+++ src/password_generation.py
@@ -0,0 +1,14 @@
+#! /bin/env python3
+
+import torch
+
+
+if torch.cuda.is_available():
+    device = torch.device("cuda:0")
+    print('CUDA AVAILABLE')
+else:
+    device = torch.device("cpu")
+    print('ONLY CPU AVAILABLE')
+
+
+
