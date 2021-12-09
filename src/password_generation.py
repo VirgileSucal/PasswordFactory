3fca1a05ee67120cd5336b8ad0278917549118af
Virgile Sucal <virgile.sucal@alumni.univ-avignon.fr>
2021-12-09 10:23:56 +0100

    LSTM training works
    Updated file:
    	password_generation.py

--- src/password_generation.py
+++ src/password_generation.py
@@ -28,8 +28,9 @@ def get_vocab(data=None):
     global __vocab_size
     assert not (__vocab is None and data is None)
     if __vocab is None:
-        __vocab = "".join(list(get_char_ratio(data)[1].keys()))
-        __vocab_size = len(__vocab) + 1  # Include EOS
+        __vocab = "\0" + "".join(list(get_char_ratio(data)[1].keys()))
+        # __vocab_size = len(__vocab) + 1  # Include EOS
+        __vocab_size = len(__vocab)
     return __vocab
 
 
@@ -62,7 +63,7 @@ class LSTM(nn.Module):
         self.dropout_value = dropout_value
         self.use_softmax = use_softmax
 
-        self.encoder = nn.Embedding(self.input_size, self.hidden_size)
+        self.encoder = nn.Embedding(self.input_size, self.hidden_size)  # input_size = vocab size, but entry is index, not one_hot
         self.lstm = nn.LSTM(
             input_size=self.hidden_size,
             hidden_size=self.hidden_size,
@@ -70,6 +71,7 @@ class LSTM(nn.Module):
             bidirectional=self.bidirectional,
             dropout=self.dropout_value,
             batch_first=True
+            # batch_first=False
         )
         self.decoder = nn.Linear(self.hidden_size, self.output_size)
 
@@ -82,42 +84,71 @@ class LSTM(nn.Module):
         # print(input)
         print(input.size())
         print(input.view(self.batch_size, 1, -1).size())
-        input = self.encoder(input.view(self.batch_size, 1, -1))
+        # input = self.encoder(input.view(self.batch_size, 1, -1))  # TODO: Bug
+        # print(self.encoder(input).size())
+        # print(torch.squeeze(self.encoder(input), 2).size())
+        # input = torch.squeeze(self.encoder(input), 2)  # TODO: Bug
+        input = self.encoder(input)
 
         # print(input)
         print(input.size())
-        output, (hidden, cell) = self.lstm(input.view(self.batch_size, 1, -1), (hidden, cell))
-        output = self.decoder(output.view(self.batch_size, 1, -1))
+        print(input.size(), (hidden.size(), cell.size()))
+        # output, (hidden, cell) = self.lstm(input.view(self.batch_size, 1, -1), (hidden, cell))
+        output, (hidden, cell) = self.lstm(input, (hidden, cell))
+        print(output.size(), (hidden.size(), cell.size()))
+        print(output.reshape(self.batch_size, 1, -1).size())
+        # output = self.decoder(output.view(self.batch_size, 1, -1))
+        # output = self.decoder(output.reshape(self.batch_size, -1))
+        output = self.decoder(output)
+        print(output.size())
+        # print(output)
 
         if bool(self.dropout_value):
             output = self.dropout(output)
         if self.use_softmax:
             output = self.softmax(output)
+        print(output.size())
+        # print(output)
+
+        print("forward ok")
 
         return output, (hidden, cell)
 
     def init_h_c(self):
         return Variable(torch.zeros(
-            self.batch_size,
             (1 + int(self.bidirectional)) * self.n_layers,
+            self.batch_size,
             self.hidden_size,
             device=device
-        ))
+        ))#.long()
 
 
 def input_tensor(line):
-    tensor = torch.zeros(len(line), 1, get_vocab_size())
-    for li in range(len(line)):
-        letter = line[li]
-        tensor[li][0][get_vocab().find(letter)] = 1
-    return tensor
+    # # tensor = torch.zeros(len(line), 1, get_vocab_size())
+    # tensor = torch.zeros(len(line), get_vocab_size())
+    # for li in range(len(line)):
+    #     letter = line[li]
+    #     # tensor[li][0][get_vocab().find(letter)] = 1
+    #     tensor[li][get_vocab().find(letter)] = 1
+    # return tensor
+    return torch.LongTensor([get_vocab().find(line[li]) for li in range(0, len(line))])
 
 
 def target_tensor(line):
-    print(line, line[0])
-    letter_indexes = [get_vocab().find(line[li]) for li in range(1, len(line))]
-    letter_indexes.append(get_vocab_size() - 1)  # EOS
-    return torch.LongTensor(letter_indexes)
+    # letter_indexes = [get_vocab().find(line[li]) for li in range(1, len(line))]
+    # # letter_indexes.append(get_vocab_size() - 1)  # EOS
+    # letter_indexes.append(0)  # EOS
+    # return torch.LongTensor(letter_indexes)
+    # tensor = torch.zeros(len(line), 1, get_vocab_size())
+    tensor = torch.zeros(len(line), get_vocab_size(), dtype=torch.float64)
+    for li in range(1, len(line)):  # Don't encode first one because it doesn't need to be predicted
+        letter = line[li]
+        # tensor[li][0][get_vocab().find(letter)] = 1
+        tensor[li][get_vocab().find(letter)] = 1.0
+        # The last one remains a 0 tensor (EOS).
+    return tensor
+    # return tensor.type(torch.float64)
+    # return torch.FloatTensor.float(tensor)
 
 
 def random_batch(passwords, batch_size=1):
@@ -128,15 +159,17 @@ def random_batch(passwords, batch_size=1):
     passwords_batch = passwords[index: index + batch_size]
     print(passwords_batch)
     print([len(p) for p in passwords_batch])
-    input_batch = torch.stack([input_tensor(password) for password in passwords_batch]).long()
-    # TODO: fix different lengths
-    print(input_batch.size())
-    target_batch = torch.stack([target_tensor(password) for password in passwords_batch]).long()
+    input_batch = pad_sequence([input_tensor(password) for password in passwords_batch], batch_first=True).long()
+    print("pad input:", input_batch.size())
+    target_batch = pad_sequence([target_tensor(password) for password in passwords_batch], batch_first=True).long()
+    print("pad target:", target_batch.size())
+    print(target_batch)
     return input_batch, target_batch
 
 
-def train_lstm_epoch(model, input_batch, target_batch, criterion, learning_rate):
-    target_batch.unsqueeze_(-1)
+# def train_lstm_epoch(model, input_batch, target_batch, criterion, learning_rate):
+def train_lstm_epoch(model, batches, criterion, learning_rate):
+    # target_batch.unsqueeze_(-1)
     hidden = model.init_h_c()
     cell = model.init_h_c()
     output = None
@@ -145,18 +178,34 @@ def train_lstm_epoch(model, input_batch, target_batch, criterion, learning_rate)
 
     loss = 0
 
-    for i in range(input_batch.size(0)):
-        # print(input_batch)
-        output, (hidden, cell) = model(input_batch[i].to(device), hidden, cell)
-        l = criterion(output.to(device), target_batch[i].to(device))
+    # for i in range(input_batch.size(0)):
+    #     # print(input_batch)
+    #     output, (hidden, cell) = model(input_batch[i].to(device), hidden, cell)
+    #     l = criterion(output.to(device), target_batch[i].to(device))
+    #     loss += l
+
+    for input_batch, target_batch in batches:
+        output, (hidden, cell) = model(input_batch.to(device), hidden, cell)
+        print("output:", output.size())
+        print("target:", target_batch.size())
+        print(target_batch.dtype)
+        # print(target_batch.type(torch.float64).dtype)
+        print(target_batch.type(torch.FloatTensor).dtype)
+        # print(target_batch.type(torch.float64))
+        print(target_batch.type(torch.FloatTensor))
+        # l = criterion(output.to(device), target_batch.to(device))
+        l = criterion(output.to(device), target_batch.type(torch.FloatTensor).to(device))
+        print("l:", type(l))
         loss += l
+        print("loss:", type(loss))
 
     loss.backward()
 
     for p in model.parameters():
         p.data.add_(p.grad.data, alpha=-learning_rate)
 
-    return output, loss.item() / input_batch.size(0)
+    # return output, loss.item() / input_batch.size(0)
+    return output, loss.item() / len(batches)
 
 
 def train_lstm(lstm, train_data, n_epochs, criterion, learning_rate):
@@ -171,9 +220,10 @@ def train_lstm(lstm, train_data, n_epochs, criterion, learning_rate):
     print_every = n_epochs / 100
 
     for iter in range(1, n_epochs + 1):
-        output, loss = train_lstm_epoch(lstm, *random_batch(train_data, batch_size=batch_size), criterion, learning_rate)
+        output, loss = train_lstm_epoch(lstm, [random_batch(train_data, batch_size=batch_size)], criterion, learning_rate)
+        print("loss 2:", type(loss))
         total_loss += loss
-        if loss[0] < best_loss:
+        if loss < best_loss[0]:
             best_loss = (loss, iter)
         all_losses.append(loss)
 
@@ -183,49 +233,53 @@ def train_lstm(lstm, train_data, n_epochs, criterion, learning_rate):
 
 if __name__ == '__main__':
 
+    train_set, eval_set = extract_data()
+    print(get_vocab(train_set))
+    get_vocab(train_set)  # Init vocab
+
+    input_size = get_vocab_size()
     hidden_size = 256
+    # hidden_size = input_size
+    output_size = get_vocab_size()
     batch_size = 1
     batch_size = 3
     n_layers = 1
     bidirectional = False
     dropout_value = 0
     use_softmax = False
+    use_softmax = True
     n_epochs = 1
     criterion = nn.CrossEntropyLoss()
     learning_rate = 0.005
-
-    train_set, eval_set = extract_data()
-    get_vocab(train_set)  # Init vocab
-    print(get_vocab(train_set))
 
     train_set = train_set[-1000:]
     # print('train_set: ', len(train_set))
     # print('eval_set: ', len(eval_set))
     # print(small_train_set)
 
-    train_dataloader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
-    eval_dataloader = DataLoader(eval_set, batch_size=batch_size, shuffle=True)
-    train_iterator = BucketIterator(
-        train_dataloader,
-        batch_size,
-    # train_iterator, eval_iterator = BucketIterator.splits(
-    #     (train_dataloader, eval_dataloader),
-    #     (batch_size, batch_size),
-        sort_key=lambda s: len(s),
-        shuffle=False,
-        sort=False,
-        device=device
-    )
-
-    print(len(train_iterator))
-    train_iterator.create_batches()
-    for batch in train_iterator.batches:
-        print(batch)
+    # train_dataloader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
+    # eval_dataloader = DataLoader(eval_set, batch_size=batch_size, shuffle=True)
+    # train_iterator = BucketIterator(
+    #     train_dataloader,
+    #     batch_size,
+    # # train_iterator, eval_iterator = BucketIterator.splits(
+    # #     (train_dataloader, eval_dataloader),
+    # #     (batch_size, batch_size),
+    #     sort_key=lambda s: len(s),
+    #     shuffle=False,
+    #     sort=False,
+    #     device=device
+    # )
+    #
+    # print(len(train_iterator))
+    # train_iterator.create_batches()
+    # for batch in train_iterator.batches:
+    #     print(batch)
 
     lstm1 = LSTM(
-        input_size=get_vocab_size(),
+        input_size=input_size,
         hidden_size=hidden_size,
-        output_size=get_vocab_size(),
+        output_size=output_size,
         batch_size=batch_size,
         n_layers=n_layers,
         bidirectional=bidirectional,
