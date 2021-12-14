711de815bc90c4a7d46f9a3563ebb05b09563cac
Virgile Sucal <virgile.sucal@alumni.univ-avignon.fr>
2021-12-14 17:30:19 +0100

    Fix bugs in evaluation
    Updated file:
    	password_generation.py

--- src/password_generation.py
+++ src/password_generation.py
@@ -1,4 +1,5 @@
 #! /bin/env python3
+import math
 import string
 import sys
 from random import randint
@@ -10,7 +11,6 @@ from torch.utils.data import DataLoader
 from torchtext.legacy.data import BucketIterator
 from torch.nn.utils.rnn import pad_sequence
 from data_mining import get_char_ratio
-from nameGeneration import timeSince, max_length, progressPercent, timeSinceStart, getLines
 from tools import extract_data
 
 from sklearn.model_selection import RandomizedSearchCV
@@ -30,6 +30,14 @@ device = torch.device("cpu")
 
 __vocab = None
 __vocab_size = None
+
+
+def time_since(since):
+    now = time()
+    s = now - since
+    m = math.floor(s / 60)
+    s -= m * 60
+    return '%dm %ds' % (m, s)
 
 
 def get_vocab(data=None):
@@ -186,7 +194,7 @@ def train_lstm(lstm, train_dataloader, n_epochs, criterion, learning_rate, print
     best_loss = (100, 0)
     n_iters = len(train_dataloader)
     epoch_size = n_iters // min(n_epochs, n_iters)
-    print(n_iters, epoch_size, n_epochs)
+    print("Data size:", n_iters, "; Epoch size:", epoch_size, "; Epochs:", n_epochs, "Batch size:", lstm.batch_size)
     if print_every is None:
         print_every = epoch_size
 
@@ -203,6 +211,10 @@ def train_lstm(lstm, train_dataloader, n_epochs, criterion, learning_rate, print
             epoch += 1
 
             batches = init_batches(current_epoch_batches)
+            # if len(batches) == 1 and batches[0].size(0) != lstm.init_h_c().size()[1]:
+            if batches[0][0].size(0) != lstm.init_h_c().size()[1]:
+                continue
+
             output, loss = train_lstm_epoch(lstm, batches, criterion, learning_rate)
             total_loss += loss
             if loss < best_loss[0]:
@@ -210,7 +222,7 @@ def train_lstm(lstm, train_dataloader, n_epochs, criterion, learning_rate, print
             all_losses.append(loss)
 
             if iter % print_every == 0:
-                print('%s (%d %d%%) %.4f (%.4f)' % (timeSince(start), epoch, epoch / n_epochs * 100, total_loss / iter, loss))
+                print('%s (%d %d%%) %.4f (%.4f)' % (time_since(start), epoch, iter / n_iters * 100, total_loss / iter, loss))
 
             current_epoch_batches = []
 
@@ -239,11 +251,11 @@ def progress(total, acc, start, epoch, l):
     else:
         bar = '=' * (filled_len - 1) + '>' + ' ' * (bar_len - filled_len)
 
-    sys.stdout.write('[%s] %s%s epoch: %d ; acc: %.3f %% ; size = %d names => coverage of %.3f %% on %s \r' % (bar, percents, '%', epoch, (100 * acc / epoch), l, (100 * acc / l), timeSinceStart(start)))
+    sys.stdout.write('[%s] %s%s epoch: %d ; acc: %.3f %% ; size = %d names => coverage of %.3f %% on %s \r' % (bar, percents, '%', epoch, (100 * acc / epoch), l, (100 * acc / l), time_since(start)))
     sys.stdout.flush()
 
 
-def generate_passwords(decoder, start_letter: str):
+def generate_passwords(decoder, start_letter: str, max_length: int = 20):
     with torch.no_grad():  # no need to track history in sampling
 
         hidden = decoder.init_h_c()
@@ -263,7 +275,9 @@ def generate_passwords(decoder, start_letter: str):
             # print(output)
             predicted_letters_indices = [argmax(letter_one_hot[0]) for letter_one_hot in output]
             # print(predicted_letters_indices)
-            if sum(predicted_letters_indices) == 0:  # If letter is EOS
+            # if sum(predicted_letters_indices) == 0:  # If letter is EOS
+            #     break
+            if predicted_letters_indices[0] == 0:  # If letter is EOS
                 break
 
             next_letters = [get_vocab()[predicted] for predicted in predicted_letters_indices]
@@ -275,18 +289,39 @@ def generate_passwords(decoder, start_letter: str):
         return output_passwords
 
 
-def test(model, nb_samples, test_data, percent):
+def get_first_letters(n, first_letters=None):
+    assert n > 0
+    if n == 1:
+        return [letter for letter in get_vocab()]
+    if first_letters is None:
+        first_letters = []
+    new_first_letters = []
+    for fl in first_letters:
+        for letter in get_vocab():
+            new_first_letters.append(fl + letter)
+    return get_first_letters(n - 1, first_letters)
+
+
+def test(model, test_data, number_of_first_letters=1):
 
     start = time()
     accuracy = 0
     nb_samples = len(test_data)
 
-    for i in range(1, nb_samples + 1):
-        random_index = randint(1, get_vocab_size() - 1)
-        starting_letter = get_vocab()[random_index]
+    print("Eval data size:", nb_samples)
+
+    # for i in range(1, nb_samples + 1):
+    i = 0
+    for starting_letter in get_first_letters(number_of_first_letters):
+        # random_index = randint(1, get_vocab_size() - 1)
+        # starting_letter = get_vocab()[random_index]
+        # # random_index = randint(1, get_vocab_size() - 1)
+        # # starting_letter += get_vocab()[random_index]
+        i += 1
 
         predicted_passwords = generate_passwords(model, starting_letter)
         # print("predicted", predicted_passwords)
+        print("predicted", predicted_passwords[0])
 
         # for predicted in predicted_passwords:
         #     if predicted in test_data:
@@ -325,18 +360,22 @@ if __name__ == '__main__':
     # hidden_size = input_size
     output_size = get_vocab_size()
     batch_size = 1
-    batch_size = 3
+    # batch_size = 3
+    batch_size = 10
+    batch_size = 64
     n_layers = 1
     bidirectional = False
     dropout_value = 0
     use_softmax = False
-    use_softmax = True
+    # use_softmax = True
     n_epochs = 1
     n_epochs = 1000
+    n_epochs = 100
+    # n_epochs = 256
     criterion = nn.CrossEntropyLoss()
     learning_rate = 0.005
     learning_rate = 0.05
-    learning_rate = 0.1
+    # learning_rate = 0.1
     print_every = 10
     print_every = None
     print_every = 1
@@ -358,11 +397,12 @@ if __name__ == '__main__':
     get_vocab(train_set)  # Init vocab
     # print(get_vocab(train_set))
 
-    # train_set = train_set[-10000:]
+    train_set = train_set[-1000:]
     # eval_set = eval_set[:100]
 
     batch_train_dataloader = DataLoader(train_set, batch_size=batch_size, shuffle=True)
-    batch_eval_dataloader = DataLoader(eval_set, batch_size=batch_size, shuffle=True)
+    # batch_eval_dataloader = DataLoader(eval_set, batch_size=batch_size, shuffle=True)
+    batch_eval_dataloader = DataLoader(eval_set, batch_size=1, shuffle=True)
 
     # print(len(batch_train_dataloader))
     # for batch in batch_train_dataloader:
@@ -383,5 +423,6 @@ if __name__ == '__main__':
 
     print("\n\nEVAL\n")
 
-    test(lstm1, len(batch_eval_dataloader), batch_eval_dataloader, 1)
+    # test(lstm1, batch_eval_dataloader)
+    test(lstm1, eval_set, 3)
 
